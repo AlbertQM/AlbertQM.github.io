5c96477313b924111c334b3d81f542ea85800a57
AlbertQM <king4spam@ymail.com>
2021-09-15 20:13:25 +0100

    Add simple animation to the "Work in progress" copy

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from "react";
+import { motion } from "framer-motion";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Work in progress.</header>
+      <header className="App-header">
+        <motion.span
+          transition={{ repeat: Infinity, duration: 2 }}
+          animate={{ rotate: [-25, 25, -25] }}
+        >
+          Work in progress.
+        </motion.span>
+      </header>
     </div>
   );
 }
